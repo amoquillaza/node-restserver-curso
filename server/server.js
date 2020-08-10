f25fd516ba6bb5c0cdabbe96196e66932c1a59ef
Arturo Moquillaza <amoquillaza@pucp.pe>
2020-08-10 00:35:44 -0500

    avance

--- server/server.js
+++ server/server.js
@@ -1,37 +1,21 @@
 require('./config/config')
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/usuario', function (req, res) {
-  res.json('get Usuario')
-})
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function (req, res) {
-    let body = req.body;
-    if(body.nombre === undefined){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({ persona : body })
-    }    
-  })
-
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({ id });
-})
-
-app.delete('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({ id });
-})
-
+mongoose.connect('mongodb://localhost:27017/cafe',
+   {    useNewUrlParser: true,    useUnifiedTopology: true}, 
+   (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando en el puerto: ', 3000);
