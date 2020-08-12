f6e2dac0a591dbbe4c55fe110e542a9089b5d9be
Arturo Moquillaza <amoquillaza@pucp.pe>
2020-08-12 01:43:36 -0500

    Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev' ){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://strider:7y5mKmAdKFrsVWqI@cluster0-p96xe.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
